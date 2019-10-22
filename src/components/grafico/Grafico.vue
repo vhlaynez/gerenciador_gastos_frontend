f02cad015d49fb7638662d5b28471abcdbb3d30d
Victor Hugo Laynez <vhlaynez@hotmail.com>
2019-10-22 16:12:45 -0300

    Adicionado primeiro gráfico

--- src/components/grafico/Grafico.vue
+++ src/components/grafico/Grafico.vue
@@ -1,9 +1,7 @@
 <template>
     <div>
         <div class="container">
-            <div id="chart">
-      
-    </div>
+            <div id="chart"></div>
         </div>
     </div>
 </template>
@@ -15,36 +13,45 @@ import ApexCharts from 'apexcharts'
 
 export default {
     name: 'Home',
-    components: {
-        apexchart: ApexCharts,
-      },
     data: function() {
         return {
-            gastos: {}
+            gastos: [],
+            series: []
         }
     },
-    
+
     methods: {
         get() {
-            const url = `${baseApiUrl}/home`
-            axios(url).then(res => (this.gastos = res.data))
+            const url = `${baseApiUrl}/grafico`
+            axios(url).then(res => {
+                this.gastos = res.data
+                var options = {
+                    theme: {
+                        palette: 'palette9'
+                    },
+
+                    series: [44, 55, 13, 43, 22],
+                    labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+                    chart: {
+                        type: 'pie'
+                    }
+                }
+                for (let i = 0; i < this.gastos.length; i++) {
+                    options.labels[i] = this.gastos[i].categoria
+                    options.series[i] = this.gastos[i].valor
+                }
+                var chart = new ApexCharts(
+                    document.querySelector('#chart'),
+                    options
+                )
+                chart.render()
+            })
         }
     },
     mounted() {
-        var options = {
-            series: [44, 55, 13, 43, 22],
-            labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
-            chart: {
-                type: 'pie'
-            },
-           series: [44, 55, 13, 43, 22],
-           
-        }
-
-        var chart = new ApexCharts(document.querySelector("#chart"), options);
-        chart.render();
-            }
+        this.get()
     }
+}
 </script>
 
 <style>
