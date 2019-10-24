6f0d7281ac4d1126cfaf8714abf7d6a195d5d418
Victor Hugo Laynez <vhlaynez@hotmail.com>
2019-10-24 13:05:41 -0300

    colocado api_key

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import './plugins/bootstrap-vue'
 import 'font-awesome/css/font-awesome.css'
 import router from './config/router'
 import './config/msgs'
+import axios from 'axios'
 import moment from 'moment'
 import money from 'v-money'
+const { authSecret } = require('../.env')
 
 Vue.use(moment)
 Vue.use(money, {
@@ -16,6 +18,17 @@ Vue.use(money, {
 })
 
 Vue.config.productionTip = false
+
+axios.interceptors.request.use(
+    async config => {
+        config.headers.api_key = authSecret
+        return config
+    },
+    error => {
+        // I cand handle a request with errors here
+        return Promise.reject(error)
+    }
+)
 
 new Vue({
     router,
